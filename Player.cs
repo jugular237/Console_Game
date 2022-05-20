2fd382437ca08a6c410dc18c11b5ddcef14619b1
jugular237 <laziutaoleksij@gmail.com>
2022-05-21 00:56:51 +0300

    made a part of game field and a bit of a character

---
+++ Player.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+public class Player
+{
+	private const int XCoord = 45;
+	private const int YCoord = 15;
+
+	private int Health { get; set; }
+	private int Ammo { get; set; }
+	enum Diresction
+    {
+		up,
+		left,
+		right
+    }
+	public void DrawPlayer()
+    {
+		Console.SetCursorPosition(XCoord, YCoord);
+		ConsoleKeyInfo keyInfo = Console.ReadKey();
+		ConsoleKey key = keyInfo.Key;
+		Diresction direction = Diresction.right;
+		
+		do
+		{
+			if(key == ConsoleKey.W && direction != Diresction.up)
+            {
+				Console.Write(@"(-_+)
+					      /\");
+				direction = Diresction.up;
+			}
+			else if (key == ConsoleKey.A && direction != Diresction.left)
+            {
+				Console.Write(@"(-_+)
+					       ̿̿ ̿̿ ̿'̿'\̵͇̿̿\/
+					      /\");
+				direction = Diresction.left;
+
+			}
+            else if (key == ConsoleKey.D && direction != Diresction.right)
+			{
+				Console.Write(@"(-_+)
+					      \==╦╦═─
+					      /\");
+				direction = Diresction.right;
+			}
+		} while (key != ConsoleKey.Enter);
+        
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -4,13 +4,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Console_Game
 {
     class Program
     {
+        private const int FieldSizeX = 95;
+        private const int FieldSizeY = 25;
         static void Main(string[] args)
         {
-            Console.WriteLine("test commit");
+            InitializeField();
+            InitializePlayer();
+            Console.ReadKey();
+        }
+
+        private static void InitializePlayer()
+        {
+            Player player = new Player();
+            player.DrawPlayer();
+        }
+
+        private static void InitializeField()
+        {
+            Console.SetWindowSize(FieldSizeX, FieldSizeY);
+            Console.SetBufferSize(FieldSizeX, FieldSizeY);
+            Console.CursorVisible = false;
+            DrawBorders();
+        }
+
+        private static void DrawBorders()
+        {
+            for(int i = 0; i < FieldSizeY; i++)
+            {
+                for (int j = 0; j < FieldSizeX; j++)
+                {
+                    if (i == 1 || i == FieldSizeY - 1)
+                    {
+                        Console.SetCursorPosition(j, i);
+                        Console.Write('-');
+                    }
+                    else if (j == 0 || j == FieldSizeX - 1)
+                    {
+                        Console.SetCursorPosition(j, i);
+                        Console.Write('|');
+                    }
+                }
+                
+            }
+                
         }
     }
 }
