7ca64fe605fe109fc3c165271204a906beb0a4ea
jugular237 <laziutaoleksij@gmail.com>
2022-05-23 01:18:30 +0300

    made lookin up animetion for player

--- Player.cs
+++ Player.cs
@@ -9,7 +9,7 @@ public class Player
 	private int Ammo { get; set; }
 	private bool isDead { get; set; }
 	
-	enum Diresction
+	public enum Diresction
     {
 		up,
 		left,
@@ -24,48 +24,71 @@ public class Player
 		{
 			ConsoleKeyInfo keyInfo = Console.ReadKey();
 			ConsoleKey key = keyInfo.Key;
-			Diresction direction=Diresction.none;
-			if (key == ConsoleKey.W && direction != Diresction.up)
+			if (key == ConsoleKey.W)
             {
-				//to do
-			}
-			else if (key == ConsoleKey.A && direction != Diresction.left)
-            {
-				ClearSpace(XCoord, YCoord);
-				Console.SetCursorPosition(XCoord, YCoord);
-				DrawPlayer(XCoord-9, YCoord, XCoord - 5, XCoord+2,
+				ClearSpace(XCoord - 17, YCoord);
+				DrawPlayer(XCoord-1, YCoord, XCoord , XCoord + 2, true,
 					new DrawHero
 					{
-						Head = @"0======* (▀ ͜ʖ▀)",
+						OverHead2 = @"||",
+						OverHead1 = @"oo",
+						Head =   @"(▀ ͜ʖ▀)  \\",
+						BodyNGun = @"|   | /",
+						Legs = @"/ \"
+					});
+				Shooting(0,0, Diresction.up);
+			}
+			else if (key == ConsoleKey.A)
+            {
+				ClearSpace(XCoord-17, YCoord);
+				DrawPlayer(XCoord-9, YCoord, XCoord - 5, XCoord+2, false,
+					new DrawHero
+					{
+						Head = @"8======* (▀ ͜ʖ▀)",
 						BodyNGun = @"\\ -- |   |",
 						Legs = @"/ \"
 					});
-				direction = Diresction.left;
+				Shooting(0,0,Diresction.left);
 			}
-            else if (key == ConsoleKey.D && direction != Diresction.right)
+            else if (key == ConsoleKey.D)
 			{
 				ClearSpace(XCoord - 17, YCoord);
-				Console.SetCursorPosition(XCoord, YCoord);
-				DrawPlayer(XCoord, YCoord, XCoord + 1, XCoord + 2,
+				DrawPlayer(XCoord, YCoord, XCoord + 1, XCoord + 2, false,
 					new DrawHero
 					{
-						Head = @"(▀ ͜ʖ▀) *======0",
+						Head = @"(▀ ͜ʖ▀) *======8",
 						BodyNGun = @"|   | -- //",
 						Legs = @"/ \"
 					});
-				direction = Diresction.right;
+				Shooting(0,0, Diresction.right);
 			}
 		} while (isDead == false);
         
     }
 
-	private void DrawPlayer(int x1Coord, int y1Coord, int x2Coord, int x3Coord, DrawHero drawhero)
+	public void Shooting(int xCoord, int yCoord, Diresction direction)
     {
+		//while () ... to do
+    }
+
+	private void DrawPlayer(int x1Coord, int y1Coord, int x2Coord, int x3Coord, bool hasOverHead, DrawHero drawhero)
+    {
+        if (hasOverHead)
+        {
+			Console.SetCursorPosition(x1Coord + 8, y1Coord - 1);
+			Console.WriteLine(@drawhero.OverHead2);
+			Console.SetCursorPosition(x1Coord + 8, y1Coord - 2);
+			Console.WriteLine(@drawhero.OverHead2);
+			Console.SetCursorPosition(x1Coord + 8, y1Coord - 3);
+			Console.WriteLine(@drawhero.OverHead2);
+			Console.SetCursorPosition(x1Coord + 8, y1Coord - 4);
+			Console.WriteLine(@drawhero.OverHead1);
+		}
 		Console.SetCursorPosition(x1Coord, y1Coord);
 		Console.WriteLine(@drawhero.Head);
-		Console.SetCursorPosition(x2Coord, y1Coord+1);
+		Console.SetCursorPosition(x2Coord, y1Coord + 1);
 		Console.WriteLine(@drawhero.BodyNGun);
-		Console.SetCursorPosition(x3Coord, y1Coord+2);
+		Console.SetCursorPosition(x3Coord, y1Coord + 2);
 		Console.WriteLine(@drawhero.Legs);
 		Console.SetCursorPosition(XCoord, YCoord);
 	}
@@ -73,17 +96,29 @@ public class Player
 	private void ClearSpace(int xCoord, int yCoord)
     {
 		Console.SetCursorPosition(xCoord, yCoord);
-		Console.Write("                 ");
+		Console.Write("                                 ");
 		Console.SetCursorPosition(xCoord, yCoord+1);
-		Console.Write("                 ");
+		Console.Write("                                ");
 		Console.SetCursorPosition(xCoord, yCoord + 2);
-		Console.Write("         ");
+		Console.Write("                 ");
+		Console.SetCursorPosition(xCoord, yCoord - 1);
+		Console.Write("                          ");
+		Console.SetCursorPosition(xCoord, yCoord - 2);
+		Console.Write("                          ");
+		Console.SetCursorPosition(xCoord, yCoord - 3);
+		Console.Write("                          ");
+		Console.SetCursorPosition(xCoord, yCoord - 4);
+		Console.Write("                          ");
 	}
 
 }
 
  public struct DrawHero
  {
+	public string OverHead1 { get; set; }
+	public string OverHead2 { get; set; }
+
+	
 	public string Head { get; set; }
 	public string BodyNGun { get; set; }
 	public string Legs { get; set; }
